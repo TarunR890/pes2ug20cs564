7c0ddeb307b499caf358b6579b98e9c99b74aca6
TarunR890 <118292671+TarunR890@users.noreply.github.com>
2023-02-16 19:09:21 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "I have added successfully the web hook\n";
+  cout << "I have added successfully the web hooks\n";
   return 0;
 }
